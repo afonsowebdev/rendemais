c7657c525ab3bb2e918dcf599fca7da5588ae60d
Francisco Afonso <afonso.webdev@gmail.com>
2026-07-12 07:45:35 +0100

    feat: adicionado novo icone de multe-contas

--- dist/assets/js/icons.js
+++ dist/assets/js/icons.js
@@ -35,7 +35,7 @@ const BX = {
   arrowDown: "bx-down-arrow-alt",
   chevR: "bx-chevron-right",
   info: "bx-info-circle",
-  bank: "bx-landmark",
+  bank: "bx-buildings",
   link: "bx-link",
   camera: "bx-camera",
   sync: "bx-sync",
